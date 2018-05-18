2256d212fbe19f74398b69796da148487352a8b1
Feifei Jiang <Feifei.Jiang@outlook.com>
2018-05-18 17:20:52 +0800

    Add

---
+++ Events/Events/EventCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Events
+{
+    public enum EventSource
+    {
+        Application,
+        Page,
+        MasterPage,
+        Control,
+    }
+
+    public class EventDescription
+    {
+        public EventSource Source { get; set; }
+        public String Type { get; set; }
+    }
+
+    public class EventCollection
+    {
+        private static List<EventDescription> events = new List<EventDescription>();
+
+        public static void Add(EventSource level, String type)
+        {
+            events.Add(new EventDescription()
+            {
+                Source = level,
+                Type = type,
+            });
+            System.Diagnostics.Debug.WriteLine($"Event:{level},{type}");
+        }
+
+        public static IEnumerable<EventDescription> Events
+        {
+            get
+            {
+                return events;
+            }
+        }
+    }
+}
---
+++ Events/Events/Global.asax.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Events
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        public Global()
+        {
+            BeginRequest += this.HandlerEvent;
+            EndRequest += this.HandlerEvent;
+            AcquireRequestState += this.HandlerEvent;
+            PostAcquireRequestState += this.HandlerEvent;
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            EventCollection.Add(EventSource.Application, "Start");
+            Application["message"] = "Application Events";
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            EventCollection.Add(EventSource.Application, "BeginRequest");
+            Response.Write(String.Format($"Request start at {DateTime.Now.ToLongTimeString()}"));
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+            EventCollection.Add(EventSource.Application, "End");
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void HandlerEvent(Object sender, EventArgs e)
+        {
+            String eventName = "<UnKnow>";
+            switch (Context.CurrentNotification)
+            {
+                case RequestNotification.BeginRequest:
+                    break;
+                case RequestNotification.EndRequest:
+                    break;
+                case RequestNotification.AcquireRequestState:
+                    break;
+                    
+            }
+        }
+    }
+}
